f5c53292c4a6642c61bf07968a0c8635e393edc9
Italo Josep <italojosep@gmail.com>
2019-11-24 16:31:02 -0300

    styles and home page

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use('/styles', express.static('styles'));
+app.use('/assets', express.static('assets'));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
 app.listen(4200, () => {
     console.log('Server running on port 4200')
     console.log('Click to acess http:localhost:4200')
+})
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/show', (req, res) => {
+    console.log(req.body)
 })
