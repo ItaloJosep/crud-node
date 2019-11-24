bcb9742667dced48591564138e616ae6dbb60906
Italo Josep <italojosep@gmail.com>
2019-11-24 18:20:39 -0300

    Create car

--- server.js
+++ server.js
@@ -2,21 +2,44 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const MongoClient = require('mongodb').MongoClient;
+
 app.use('/styles', express.static('styles'));
 app.use('/assets', express.static('assets'));
 
+const uri = "mongodb+srv://admin:teste123@luxury-car-voah6.mongodb.net/test?retryWrites=true&w=majority"
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('luxury-car')
+
+    app.listen(4200, () => {
+        console.log('Server running on port 4200')
+        console.log('Click to acess http:localhost:4200')
+    })
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
 
-app.listen(4200, () => {
-    console.log('Server running on port 4200')
-    console.log('Click to acess http:localhost:4200')
-})
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('index.ejs', { data: results })
+    })
 })
 
-app.post('/show', (req, res) => {
-    console.log(req.body)
+app.get('/administrator/cadastro', (req, res) => {
+    res.render('cadastro.ejs')
+})
+
+app.post('/cadastrar', (req, res) => {
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/')
+    })
 })
