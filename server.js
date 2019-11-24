927636b81acfa6143cfdaaa9f18a2c8bfdded482
Italo Josep <italojosep@gmail.com>
2019-11-24 20:20:02 -0300

    Page edit

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
 
-
 app.get('/', (req, res) => {
     db.collection('data').find().toArray((err, results) => {
         if (err) return console.log(err)
@@ -31,15 +30,66 @@ app.get('/', (req, res) => {
     })
 })
 
+//ADMINISTRATOR
+app.get('/administrator', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('administrator.ejs', { data: results })
+    })
+})
+
+// CREATE NEW CAR
 app.get('/administrator/cadastro', (req, res) => {
     res.render('cadastro.ejs')
 })
 
-app.post('/cadastrar', (req, res) => {
+app.post('/administrator/cadastrar', (req, res) => {
     db.collection('data').save(req.body, (err, result) => {
         if (err) return console.log(err)
 
         console.log('Salvo no Banco de Dados')
-        res.redirect('/')
+        res.redirect('/administrator')
+    })
+})
+
+// EDIT CAR
+app.route('/edit/:id').get((req, res) => {
+    var ObjectId = require('mongodb').ObjectID;
+    var id = req.params.id
+
+    db.collection('data').find({ _id: ObjectId.createFromHexString(id) }).toArray((err, result) => {
+        if (err) return res.send(err)
+        console.log("=========")
+        console.log(result)
+        console.log("=========")
+        res.render('editar.ejs', { data: result })
+    })
+
+}).post((req, res) => {
+    var ObjectId = require('mongodb').ObjectID;
+    var id = req.params.id
+    var marca = req.body.marca
+    var modelo = req.body.modelo
+    var cor = req.body.cor
+    var ano = req.body.ano
+    var preco = req.body.preco
+    var descricao = req.body.descricao
+    var urlfoto = req.body.urlfoto
+
+
+
+    db.collection('data').updateOne({ _id: ObjectId.createFromHexString(id) }, {
+        $set: {
+            marca: marca,
+            modelo: modelo,
+            cor: cor,
+            ano: ano,
+            preco: preco,
+            descricao: descricao,
+            urlfoto: urlfoto
+        }
+    }, (err, result) => {
+        if (err) return res.send(err)
+        res.redirect('/administrator')
     })
 })
